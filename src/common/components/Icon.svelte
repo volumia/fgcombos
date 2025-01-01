352652b3747be6e9ca22073e1f3472229dcc976d
ehnba <getehnba@gmail.com>
2025-01-01 18:14:19 +0300

    1. Add Icon component. 2. Each move in the SelectMoveModal displays their damage and proration values, with corresponding icons.

---
+++ src/common/components/Icon.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	type Props = {
+		path?: string;
+		src?: string;
+		size?: number | string;
+		color?: string;
+		flip?: string;
+		rotate?: number;
+		marginBottom?: number;
+		marginRight?: number;
+		title?: string | undefined;
+		className?: string;
+	};
+
+	let {
+		path = undefined,
+        src = undefined,
+		size = 1,
+		color = undefined,
+		flip = undefined,
+		rotate = 0,
+		marginBottom = 0,
+		marginRight = 0,
+		title = undefined,
+		className = ""
+	}: Props = $props();
+
+	if (Number(size)) size = Number(size);
+
+    const fromFile = typeof src === "string";
+    const width = $derived(typeof size === "string" ? size : `${size * 1.5}em`);
+    
+	const getStyles = () => {
+		const transform = [];
+		const styles = [];
+
+		styles.push(["width", width]);
+		styles.push(["height", width]);
+
+        if (fromFile) {
+            styles.push(["-webkit-mask", `url(${src}) no-repeat center`]);
+            styles.push(["mask", `url(${src}) no-repeat center`]);
+            styles.push(["background-color", color !== undefined ? color : "currentColor"]);
+            styles.push(["display", "inline-block"]);
+            styles.push(["vertical-align", "middle"]);
+        }
+        else {
+            styles.push(["fill", color !== undefined ? color : "currentColor"]);
+        }
+
+		if (flip === "h") {
+			transform.push("scaleX(-1)");
+		}
+		if (flip === "v") {
+			transform.push("scaleY(-1)");
+		}
+		if (rotate != 0) {
+			transform.push(`rotate(${rotate}deg)`);
+		}
+		if (transform.length > 0) {
+		    styles.push(["transform", transform.join(" ")]);
+			styles.push(["transform-origin", "center"]);
+		}
+		if (marginBottom > 0) {
+			styles.push(["margin-bottom", `${marginBottom}em`]);
+		}
+		if (marginRight > 0) {
+			styles.push(["margin-right", `${marginRight}em`]);
+		}
+		return styles.reduce((cur, item) => {
+			return `${cur} ${item[0]}:${item[1]};`;
+		}, "");
+	};
+
+	let style: string = $derived(getStyles());
+</script>
+
+{#if src != undefined}
+    <div {style} class={className}></div>
+{:else}
+    <svg viewBox="0 0 24 24" {style} class={className}>
+        {#if title != undefined}
+            <title>{title}</title>
+        {/if}
+        <path d={path} />
+    </svg>
+{/if}
+
+<style>
+	svg {
+		vertical-align: middle;
+		display: inline-block;
+	}
+</style>
--- src/routes/SelectMoveModal.svelte
+++ src/routes/SelectMoveModal.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+    import Fuse from "fuse.js";
 	import type { Move, Moveset } from "@/moveTypes";
+    import type { Action } from "svelte/action";
+    import Icon from "@/common/components/Icon.svelte";
 
     type Props = {
         moveset: Moveset;
@@ -7,28 +10,122 @@
     }
 
     let { moveset, onAddMove }: Props = $props();
+    let searchQuery: string = $state("");
+    const fuse = new Fuse(moveset.moves, {
+        threshold: 0.2,
+        keys: ["name", "notation"]
+    });
+    let selectableMoves: Move[] = $derived(getFilteredMoves(searchQuery));
+    let isSearchEmpty = $derived(searchQuery.trim() === "");
+
+    function getFilteredMoves(query: string): Move[] {
+        if (isSearchEmpty) {
+            return moveset.moves
+        }
+        else {
+            const result = fuse.search<Move>(query);
+            return result.map(r => r.item);
+        }
+    }
+
+    function onSubmitSearch(e: SubmitEvent) {
+        e.preventDefault();
+
+        if (!isSearchEmpty && selectableMoves.length > 0) {
+            onAddMove(selectableMoves[0]);
+        }
+    }
 </script>
 
 <div class="modal">
-    {#each moveset.moves as move }
-        <button onclick={() => onAddMove(move)}>{move.name}&nbsp;{move.notation}</button>
+    <form onsubmit={onSubmitSearch}>
+        <input 
+            type="search" 
+            id="search" 
+            name="search" 
+            placeholder="Search moves" 
+            autocomplete="off"
+            autofocus={true}
+            bind:value={searchQuery}
+        />
+        <input type="submit" hidden />
+    </form>
+
+    {#each selectableMoves as move, i}
+        <button 
+            class={i == 0 && !isSearchEmpty ? "move selected" : "move"}
+            onclick={() => onAddMove(move)}
+        >
+            <div class="line">
+                <div>{move.notation}</div>
+                <div class="name">{move.name}</div>
+                <div class="filler"></div>
+                <div class="damage">
+                    <Icon src={"./icons/fist.svg"}></Icon> 
+                    {move.baseDamage}
+                </div>
+                <div class="proration">
+                    <Icon src={"./icons/trend-down.svg"}></Icon> 
+                    {move.proration*100}%
+                </div>
+            </div>
+        </button>
     {/each}
 </div>
 
 <style lang="scss">
-    @use "@/style/variables" as v;
-    
+    @use "@/style/variables" as *;
     .modal {
-        max-width: 200px;
-        padding: v.$spacing-1;
+        width: fit-content;
+        padding: $spacing-1;
+
+        font-size: 1em;
 
         border: 1px solid black;
         background-color: gray;
     }
 
-    button {
-        display: block;
+    #search {
+        width: 100%;
+        font-size: 1em;
+    }
 
-        margin: v.$spacing-1;
+    button.move {
+        display: block;
+        
+        width: 100%;
+        margin: $spacing-1 0;
+        font-size: 1em;
+
+        text-align: start;
+
+        &.selected {
+            outline: 1px solid rgb(0, 219, 183);
+        }
+
+        .line {
+            display: flex;
+            flex-direction: row;
+            gap: .5ch;
+
+            .name {
+                font-size: .8em;
+                color: rgb(73, 73, 73);
+            }
+
+            .filler {
+                flex-grow: 1;
+            }
+
+            .damage, .proration {
+                min-width: 6ch;
+                font-size: .8em;
+                color: rgb(73, 73, 73);
+            }
+        }
+    }
+
+    svg {
+        color: inherit;
     }
 </style>
