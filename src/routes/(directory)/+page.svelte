7c62f6c39803b6d22da13d90feb6e9006eb06b3e
volumia <getehnba@gmail.com>
2025-01-06 21:57:41 +0300

    routes/(directory): Improve layout by adding clickable character cards that link to each character's combo page.

--- src/routes/(directory)/+page.svelte
+++ src/routes/(directory)/+page.svelte
@@ -1,2 +1,37 @@
-<h2>Street Fighter III: 3rd Strike</h2>
-<a href="/combo/ryu-sf3ts">Ryu</a>
+<script lang="ts">
+    import CharacterCard from "./CharacterCard.svelte";
+</script>
+
+<p class="center-horizontally">Choose a character</p>
+<p class="center-horizontally">↓</p>
+<h1>Street Fighter III - 3rd Strike</h1>
+<div class="card-grid">
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+    <CharacterCard characterId="ryu-sf3ts"></CharacterCard>
+</div>
+
+<style lang="scss">
+    .card-grid {
+        display: flex;
+        width: 100%;
+
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: start;
+        gap: 16px;
+    }
+
+    .center-horizontally {
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
