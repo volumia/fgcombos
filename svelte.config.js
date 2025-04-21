849b1ac73ac4bb66972ddd094189b9065b19f41d
volumia <getehnba@gmail.com>
2025-04-21 22:42:11 +0300

    Setup Cloudflare Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,30 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({script: true})],
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		alias: {
-			"@/*": "./src/*",
-			"#/*": "./static/*",
-		}
-	}
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: [vitePreprocess({ script: true })],
+    kit: {
+        adapter: adapter({
+            config: undefined,
+            platformProxy: {
+                configPath: undefined,
+                environment: undefined,
+                persist: undefined
+            },
+            fallback: 'plaintext',
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        }),
+        alias: {
+            '@/*': './src/*',
+            '#/*': './static/*'
+        }
+    }
 };
 
 export default config;
